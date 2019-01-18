dfe4888c3a57f3e031f984ec8e11b8d1b00791ab
Gowry Sugathan <gowrysugathan18@gmail.com>
2019-01-18 21:05:35 +0530

    Add variables

--- Auction.sol
+++ Auction.sol
@@ -1,17 +1,19 @@
- 
 contract Auction{
-
+    
+    address auctionOwner;
+    uint incrementBid;
+    address highestBidder;
+    uint highestBid;
+    uint bindingBid;
     //allows user to place bids
     function placeBid() public returns (bool success){
         
     }
-    
     /*allows owner to withdraw fund from the contract after
     the aucton is finished*/
     function withdrawFund() public returns (bool success){
         
     }
-    
     /*allows owner to cancel the auction in any case of emergency*/
     function cancelAuction() public returns (bool success){
         
