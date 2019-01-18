ebab302cb3ae534ae42e8f83814ea5f84e825189
Gowry Sugathan <gowrysugathan18@gmail.com>
2019-01-18 20:36:05 +0530

    first commit

---
+++ Auction.sol
@@ -0,0 +1,19 @@
+ 
+contract Auction{
+
+    //allows user to place bids
+    function placeBid() public returns (bool success){
+        
+    }
+    
+    /*allows owner to withdraw fund from the contract after
+    the aucton is finished*/
+    function withdrawFund() public returns (bool success){
+        
+    }
+    
+    /*allows owner to cancel the auction in any case of emergency*/
+    function cancelAuction() public returns (bool success){
+        
+    }
+}
