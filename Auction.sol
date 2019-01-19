05dde40539011cef0c6f6f094823ca73691301a2
Gowry Sugathan <gowrysugathan18@gmail.com>
2019-01-19 18:57:24 +0530

    Add place bid function

--- Auction.sol
+++ Auction.sol
@@ -4,19 +4,31 @@ contract Auction{
     uint incrementBid;
     address highestBidder;
     uint highestBid;
-    uint bindingBid;
+    uint highestBindingBid;
+    mapping(address=>uint256) fundOfBidder;
     
     /*constructor to initialize the owner and the bid increment 
     value if the address of the auction owner is filled, i.e not 0*/
-    function Auction(address _auctionOwner, uint _incrementBid){
+    function Auction(address _auctionOwner, uint _incrementBid ,uint minBid){
         require(_auctionOwner!=0 );
         auctionOwner = _auctionOwner;
         incrementBid = _incrementBid;
+        highestBid = minBid;
+        highestBindingBid = 10;
+        highestBidder = auctionOwner;
     }
     
     //allows user to place bids
     function placeBid() public returns (bool success){
-        
+        require(msg.sender!=auctionOwner);
+        require(msg.value != 0);
+        uint newBid = fundOfBidder[msg.sender]+msg.value;
+        if(newBid<highestBid){
+            revert();
+        }
+        if(msg.sender == highestBidder){
+            highestBid = newBid;
+        }
     }
     /*allows owner to withdraw fund from the contract after
     the aucton is finished*/
