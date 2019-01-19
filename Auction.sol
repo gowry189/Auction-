8c044508ac7e3c98a3ff24465768ccfe96d40b73
Gowry Sugathan <gowrysugathan18@gmail.com>
2019-01-19 17:44:05 +0530

    Add constructor

--- Auction.sol
+++ Auction.sol
@@ -5,6 +5,15 @@ contract Auction{
     address highestBidder;
     uint highestBid;
     uint bindingBid;
+    
+    /*constructor to initialize the owner and the bid increment 
+    value if the address of the auction owner is filled, i.e not 0*/
+    function Auction(address _auctionOwner, uint _incrementBid){
+        require(_auctionOwner!=0 );
+        auctionOwner = _auctionOwner;
+        incrementBid = _incrementBid;
+    }
+    
     //allows user to place bids
     function placeBid() public returns (bool success){
         
